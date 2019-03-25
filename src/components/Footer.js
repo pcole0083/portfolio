385b7e648b6d04e0ca31daf34d9a74c88fabd68b
Patrick Coleman <pcole0083@gmail.com>
2019-03-24 23:20:01 -0400

    update to the header and footer to hide links to pages that are not yet complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,79 +20,28 @@ const Footer = class extends React.Component {
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
-              <div className="columns">
-                <div className="column is-6">
+            <div className="columns">
+              <div className="column is-12">
                 <section className="menu">
-                    <ul className="menu-list">
-                      <li><Link to="/" className="navbar-item text-right">Home</Link></li>
-                      <li><Link className="navbar-item text-right" to="/about">About</Link></li>
-                      <li>
-                        <a
-                          className="navbar-item text-right"
-                          href="/admin/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          Admin
-                        </a>
-                      </li>
-                    </ul>
-                  </section>
-                </div>
-                <div className="column is-6">
-                  <section>
-                    <ul className="menu-list">
-                      <li>
-                        <Link className="navbar-item" to="/blog">
-                          Latest Stories
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="navbar-item" to="/contact">
-                          Contact
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="navbar-item" to="/contact/examples">
-                          Form Examples
-                        </Link>
-                      </li>
-                    </ul>
-                  </section>
-                </div>
-                {/*<div className="column is-4 social">
-                  <a title="facebook" href="https://facebook.com">
-                    <img 
-                      src={facebook}
-                      alt="Facebook"
-                      style={{ width: '1em', height: '1em' }}
-                    />
-                  </a>
-                  <a title="twitter" href="https://twitter.com">
-                    <img
-                      className="fas fa-lg"
-                      src={twitter}
-                      alt="Twitter"
-                      style={{ width: '1em', height: '1em' }}
-                    />
-                  </a>
-                  <a title="instagram" href="https://instagram.com">
-                    <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                    />
-                  </a>
-                  <a title="vimeo" href="https://vimeo.com">
-                    <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                    />
-                  </a>
-                </div>*/}
+                  <ul className="menu-list">
+                    <li><Link to="/" className="navbar-item text-center">Home</Link></li>
+                    <li><Link className="navbar-item text-center" to="/about">About</Link></li>
+                    <li><Link className="navbar-item text-center" to="/contact">Contact</Link></li>
+                    {/*<li>
+                      <a
+                        className="navbar-item text-right"
+                        href="/admin/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Admin
+                      </a>
+                    </li>*/}
+                  </ul>
+                </section>
               </div>
             </div>
+          </div>
         </div>
       </footer>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,9 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/blog">
+              {/*<Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
+              </Link>*/}
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
