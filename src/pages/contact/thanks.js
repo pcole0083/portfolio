21f5ee88f92ec36c1003566e4dc64767c98329e6
Patrick Coleman <pcole0083@gmail.com>
2019-04-14 14:02:12 -0400

    some fun styles added to the footer let's parse HTML in the description thank you page better message

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,7 +7,7 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>Thank you for contacting me! I will get back to you as soon as I able, usually within 24 hours.</p>
         </div>
       </div>
     </section>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -80,7 +80,7 @@ export const IndexPageTemplate = ({
                   <h3 className="has-text-weight-semibold is-size-2">
                     {heading}
                   </h3>
-                  <p>{description}</p>
+                  <p dangerouslySetInnerHTML={{ __html: description }}></p>
                 </div>
               </div>
               <Features gridItems={intro.blurbs} />
