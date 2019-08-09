95a6e810daa8bcb3894da2bf4bc792c7d6736568
Patrick Coleman <pcole0083@gmail.com>
2019-08-09 14:31:58 -0400

    updated SEO title and descrpiption. Updated package name, description, author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'PColeman Dev - Frontend Engineer',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'I am a Frontend Engineer always looking to push my boundaries and learn something new.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
